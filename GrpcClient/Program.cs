b4a41cb830ac9a2354bd06203f8ae1c145572f23
utkucan <utkucan@github.com>
2020-07-07 22:11:26 +0300

    Proje dosyası ekle.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using GrpcServer.Protos;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // var input = new HelloRequest { Name = "Tim" };
+            //var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            //var client = new Greeter.GreeterClient(channel);
+
+            //var reply = await client.SayHelloAsync(input);
+
+            //Console.WriteLine(reply.Message);
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var customerClient = new Customer.CustomerClient(channel);
+
+            var clientRequested = new CustomerLookupModel { UserId =2 };
+
+            var customer = await customerClient.GetCustomerInfoAsync(clientRequested);
+
+            Console.WriteLine($"{customer.Firstname }{customer.Lastname }");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GrpcServer/Services/CustomersService.cs
@@ -0,0 +1,42 @@
+﻿using Grpc.Core;
+using GrpcServer.Protos;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Services
+{
+    public class CustomersService:Customer.CustomerBase
+    {
+        private readonly ILogger<CustomersService>_logger;
+        public CustomersService(ILogger<CustomersService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            CustomerModel output = new CustomerModel();
+
+            if(request.UserId==1)
+            {
+                output.Firstname = "Andrus";
+                output.Lastname = "Gerber";
+
+            }
+            else if(request.UserId==2)
+            {
+                output.Firstname = "Ingar";
+                output.Lastname = "Ashment";
+            }
+            else
+            {
+                output.Firstname = "Amalee";
+                output.Lastname = "Mcbeth";
+            }
+            return Task.FromResult(output);
+        }
+    }
+}
